8061ece4b44962f0c536ac1d656a442b483d92c3
c0a2422947 <c0a2422947@edu.teu.ac.jp>
2025-11-22 07:00:50 +0900

    練習10:こうかとんの操作実装

--- pygame_image.py
+++ pygame_image.py
@@ -15,15 +15,27 @@ def main():
     other_bg_img = pg.transform.flip(other_bg_img, True, False)
     kt_img = pg.image.load("fig/3.png")
     kt_img = pg.transform.flip(kt_img, True, False)
+    kt_rct = kt_img.get_rect()
+    kt_rct.center = 300, 200
+
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kt_rct.move_ip((0, -1))
+        elif key_lst[pg.K_DOWN]:
+            kt_rct.move_ip((0, 1))
+        elif key_lst[pg.K_LEFT]:
+            kt_rct.move_ip((-1, 0))
+        elif key_lst[pg.K_RIGHT]:
+            kt_rct.move_ip((1, 0))
         screen.blit(bg_img, [-tmr, 0])
         screen.blit(other_bg_img, [1600-tmr, 0])
         screen.blit(bg2_img, [3200-tmr, 0])
-        screen.blit(kt_img, [300, 200])
+        screen.blit(kt_img, kt_rct)
         pg.display.update()
         tmr += 1        
         clock.tick(200)
